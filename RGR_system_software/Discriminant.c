38d0e4b46c66828c02687364c939f89148d5e027
Dima Zotsenko <dmitriz156@gmail.com>
2023-06-09 23:39:51 +0300

    Work on the Bash script: Writing the algorithm for the cyclic launch of the program.

--- RGR_system_software/Discriminant.c
+++
@@ -1,7 +0,0 @@
-#include "Discriminant.h"
-
-// Функція для обчислення дискримінанту
-double discriminant(double a, double b, double c) {
-    return b*b - 4*a*c;
-}
-
--- RGR_system_software/Discriminant.h
+++
@@ -1,7 +0,0 @@
-#ifndef DISCRIMINANT_H
-#define DISCRIMINANT_H
-
-// Функція для обчислення дискримінанту
-double discriminant(double a, double b, double c);
-
-#endif // DISCRIMINANT_H
